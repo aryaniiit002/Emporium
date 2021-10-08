ae42d5fe2d9eb687fcd95ac9799ffe3332db557b
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-10-08 10:37:32 +0530

    Single Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
 
 const App = () => {
     return <Home/>;
