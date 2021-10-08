2c6c97055116c4758e9b6b24215e5e42766592f5
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-10-08 14:02:41 +0530

    Login and Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 // import Product from "./pages/Product";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
 
 const App = () => {
     return <Home/>;
